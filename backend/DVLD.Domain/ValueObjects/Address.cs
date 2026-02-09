5f2e54345074d45f889883d81c6bed0d1e5921d7
Mohamed Oualil <emohamedoualil@gmail.com>
2026-02-09 22:29:42 +0100

    Create Address value object

---
+++ backend/DVLD.Domain/ValueObjects/Address.cs
@@ -0,0 +1,52 @@
+﻿using DVLD.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD.Domain.ValueObjects
+{
+    public record class Address
+    {
+        public string Street { get; init; }
+        public string City { get; init; }
+        public string State { get; init; }
+        public string ZipCode { get; init; }
+        public string Country { get; init; }
+
+        private Address()
+        {
+            
+        }
+
+        private Address(string street, string city, string zipCode, string country)
+        {
+            Street = street;
+            City = city;
+            ZipCode = zipCode;
+            Country = country;
+
+            
+        }
+        public static Result<Address> Create(string street,string city,string ZipCode,string country)
+        {
+            if (string.IsNullOrWhiteSpace(street))
+                return Result<Address>.Failure("Street is Requeied");
+
+            if (string.IsNullOrWhiteSpace(city))
+                return Result<Address>.Failure("city is Requeied");
+
+
+            if (ZipCode.Length != 5 )
+                return Result<Address>.Failure("ZipCode Should Be 5 Characters");
+
+
+            if (string.IsNullOrWhiteSpace(city))
+                return Result<Address>.Failure("country is Requeied");
+
+            var address = new Address(street,city, ZipCode, country);
+
+        }
+    }
+}
