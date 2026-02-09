8b7727aff9debd9b360a689d588df27e2e2c0bf7
Mohamed Oualil <emohamedoualil@gmail.com>
2026-02-09 22:08:26 +0100

    Create Value Oject for NationalNo

---
+++ backend/DVLD.Domain/ValueObjects/NationalNo.cs
@@ -0,0 +1,26 @@
+﻿using DVLD.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD.Domain.ValueObjects
+{
+    public class NationalNo : ValueObject
+    {
+        public string Number { get; set; }
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public NationalNo(string number)
+        {
+            if (string.IsNullOrEmpty(number))
+                throw new ArgumentNullException("number");
+            
+
+        }
+    }
+}
