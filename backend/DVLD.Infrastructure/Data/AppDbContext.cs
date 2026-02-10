105f4eca46865fdf7223567c33d40b1021db6fa9
Mohamed Oualil <emohamedoualil@gmail.com>
2026-02-10 18:10:48 +0100

    Add ConnectionString for Database and imlement Dbcontext

---
+++ backend/DVLD.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DVLD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Person> Students => Set<Person>();
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+        }
+    }
+}
---
+++ backend/DVLD.Infrastructure/DependencyInjection/DbConfig.cs
@@ -0,0 +1,30 @@
+﻿using DVLD.Domain.Interfaces;
+using DVLD.Infrastructure.Data;
+using DVLD.Infrastructure.Repositorys;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD.Infrastructure.DependencyInjection
+{
+    public static class DbConfig
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddScoped<IPersonRepository, PersonRepository>();
+
+            return services;
+
+
+        }
+    }
+}
