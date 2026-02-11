9817e4a22be8f4fa275c7c36582ee98478928f67
Mohamed Oualil <emohamedoualil@gmail.com>
2026-02-11 12:27:52 +0100

    feat: add repository contracts and base implemention for user

--- backend/DVLD.Infrastructure/Data/Configuration/UserConfiguration.cs
+++ backend/DVLD.Infrastructure/Data/Configuration/UserConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DVLD.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,23 @@ using System.Threading.Tasks;
 
 namespace DVLD.Infrastructure.Data.Configuration
 {
-    internal class UserConfiguration
+    public class UserConfiguration : BaseEntityConfiguration<User, int>
     {
+        public override void Configure(EntityTypeBuilder<User> builder)
+        {
+            base.Configure(builder);
+            builder.ToTable("Users");
+            builder.Property(p=>p.UserName).IsRequired()
+                .HasMaxLength(20);
+            builder.HasIndex(p => p.UserName).IsUnique();
+
+            builder.Property(p => p.PasswordHash).IsRequired()
+                    .HasMaxLength(255);
+
+            builder.HasOne(u => u.Person)
+                .WithOne()
+                .HasForeignKey<User>(p => p.PersonId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
---
+++ backend/DVLD.Infrastructure/Repositorys/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using DVLD.Domain.Entities;
+using DVLD.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD.Infrastructure.Repositorys
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task<int> AddAsync(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> FindAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(User entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
