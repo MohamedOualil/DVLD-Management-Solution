6ae851a2ba8feb3275200f65d52f39fbe20c391f
Mohamed Oualil <emohamedoualil@gmail.com>
2026-02-11 11:49:21 +0100

    Implement BCryptPasswordHasher in Infrastructure using BCrypt.Net-Next

---
+++ backend/DVLD.Infrastructure/Services/BCryptPasswordHasher.cs
@@ -0,0 +1,23 @@
+﻿using DVLD.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BCrypt.Net;
+
+namespace DVLD.Infrastructure.Services
+{
+    public class BCryptPasswordHasher : IPasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+        }
+
+        public bool VerifyPassword(string password, string hash)
+        {
+            return BCrypt.Net.BCrypt.EnhancedVerify(password, hash);
+        }
+    }
+}
