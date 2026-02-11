20a3e2920ff673a4bb92509e21d16b697e21197d
Mohamed Oualil <emohamedoualil@gmail.com>
2026-02-11 15:29:42 +0100

    Created ApplicationTypes entity and implement it with Money value object

---
+++ backend/DVLD.Domain/Entities/ApplicationTypes.cs
@@ -0,0 +1,48 @@
+﻿using DVLD.Domain.Common;
+using DVLD.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD.Domain.Entities
+{
+    public class ApplicationTypes : Entity<int>
+    {
+        public string ApplicationName { get; private set; }
+        public Money ApplicationFees { get; private set; }
+
+        private ApplicationTypes()
+        {
+            
+        }
+
+        private ApplicationTypes(string applicationName, Money applicationFees)
+        {
+            ApplicationName = applicationName;
+            ApplicationFees = applicationFees;
+            
+        }
+
+        public static Result<ApplicationTypes> Create(string applicationName, Money applicationFees)
+        {
+            if (string.IsNullOrWhiteSpace(applicationName))
+                return Result<ApplicationTypes>.Failure("Application Name is required.");
+
+            if (applicationFees == null)
+                return Result<ApplicationTypes>.Failure("Application Fees are required.");
+
+
+            return Result<ApplicationTypes>.Success(new ApplicationTypes(applicationName, applicationFees));
+
+
+        }
+
+        public void UpdateFees(Money newfees)
+        {
+            if (newfees != null)
+                ApplicationFees = newfees;
+        }
+    }
+}
--- backend/DVLD.Domain/ValueObjects/Money.cs
+++ backend/DVLD.Domain/ValueObjects/Money.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DVLD.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,70 @@ using System.Threading.Tasks;
 
 namespace DVLD.Domain.ValueObjects
 {
-    internal class Money
+    public record class Money
     {
+        public decimal Amount { get; init; }
+        public string Currency { get; init; } = "USD";
+
+        private Money()
+        {
+            Amount = 0;
+            Currency = string.Empty;
+        }
+
+        private Money(decimal amount, string currency)
+        {
+            Amount = amount;
+            Currency = currency;
+            
+        }
+
+        public static Result<Money> Create(decimal amount, string currency = "USD")
+        {
+            if (amount < 0)
+                return Result<Money>.Failure("Money amount cannot be negative.");
+
+            return Result<Money>.Success(new Money(amount,currency));
+
+
+        }
+
+        public static  Result<Money> operator -(Money left,Money right)
+        {
+            if (left.Currency != right.Currency)
+                return Result<Money>.Failure("Cannot subtract different currencies.");
+
+            var newAmount = left.Amount - right.Amount;
+
+            if (newAmount < 0)
+                return Result<Money>.Failure("Resulting balance cannot be negative.");
+
+            return Result<Money>.Success(new Money(newAmount, left.Currency));
+
+        }
+
+        public static Result<Money> operator +(Money left, Money right)
+        {
+            if (left.Currency != right.Currency)
+                return Result<Money>.Failure("Cannot add money with different currencies.");
+
+            var newAmount = left.Amount + right.Amount;
+
+
+            return Result<Money>.Success(new Money(newAmount, left.Currency));
+
+        }
+
+        public static bool operator >(Money left, Money right)
+        {
+            return left.Currency == right.Currency && left.Amount > right.Amount;
+
+        }
+
+        public static bool operator <(Money left, Money right)
+        {
+            return left.Currency == right.Currency && left.Amount < right.Amount;
+
+        }
     }
 }
