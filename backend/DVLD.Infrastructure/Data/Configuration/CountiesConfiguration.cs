9c940bd818995b08f01a3f380b23b5b3f34e308c
Mohamed Oualil <emohamedoualil@gmail.com>
2026-02-11 09:44:46 +0100

    Create Counties Configuration

---
+++ backend/DVLD.Infrastructure/Data/Configuration/CountiesConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using DVLD.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD.Infrastructure.Data.Configuration
+{
+    public class CountiesConfiguration : BaseEntityConfiguration<Counties,int>
+    {
+        public override void Configure(EntityTypeBuilder<Counties> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(p =>p.CountryName).IsRequired()
+                .HasMaxLength(50);
+            builder.Property(p => p.CountryCode).IsRequired()
+                .HasMaxLength(5);
+        }
+    }
+}
