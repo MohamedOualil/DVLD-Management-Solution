f963fa25d539efe5af1beabe737b28b8f748805c
Mohamed Oualil <emohamedoualil@gmail.com>
2026-02-11 15:43:35 +0100

    feat: add repository contracts and base implemention for Application Types

---
+++ backend/DVLD.Domain/Interfaces/IApplicationTypesRepository.cs
@@ -0,0 +1,13 @@
+﻿using DVLD.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD.Domain.Interfaces
+{
+    public interface IApplicationTypesRepository : IBaseRepository<IApplicationTypesRepository>
+    {
+    }
+}
---
+++ backend/DVLD.Infrastructure/Repositorys/ApplicationTypesRepositorys.cs
@@ -0,0 +1,37 @@
+﻿using DVLD.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD.Infrastructure.Repositorys
+{
+    public class ApplicationTypesRepositorys : IApplicationTypesRepository
+    {
+        public Task<int> AddAsync(IApplicationTypesRepository entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IApplicationTypesRepository> FindAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<IApplicationTypesRepository>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(IApplicationTypesRepository entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
