bd118f53c3501ebce52c9c05bd7f0aaa3112f7c3
Mohamed Oualil <emohamedoualil@gmail.com>
2026-02-12 10:03:11 +0100

    Implemnet and Configuration Driver Entity

---
+++ backend/DVLD.Domain/Entities/Driver.cs
@@ -0,0 +1,45 @@
+﻿using DVLD.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD.Domain.Entities
+{
+    public class Driver : Entity<int>
+    {
+        public int PersonId { get; private set; }
+        public Person Person { get; private set; }
+
+        public int CreatedByUserId { get; private set; }
+        public User CreatedBy { get; private set; }
+
+
+        private Driver()
+        {
+            
+        }
+
+        private Driver(Person person, User createdBy )
+        {
+            PersonId = person.Id;
+            Person = person;
+            CreatedByUserId = createdBy.Id;
+            CreatedBy = createdBy;
+            
+        }
+
+        public static Result<Driver> Create(Person person, User createdBy)
+        {
+            if (person == null)
+                return Result<Driver>.Failure("Person Info is required.");
+
+            if (createdBy == null)
+                return Result<Driver>.Failure("User  is required.");
+
+            return Result<Driver>.Success(new Driver(person, createdBy));
+
+        }
+    }
+}
---
+++ backend/DVLD.Infrastructure/Data/Configuration/DriverConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using DVLD.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD.Infrastructure.Data.Configuration
+{
+    public class DriverConfiguration : BaseEntityConfiguration<Driver, int>
+    {
+        public override void Configure(EntityTypeBuilder<Driver> builder)
+        {
+            base.Configure(builder);
+
+            builder.HasOne(d => d.Person)
+            .WithOne() 
+            .HasForeignKey<Driver>(d => d.PersonId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+
+            builder.HasOne(d => d.CreatedBy)
+                .WithMany()
+                .HasForeignKey(d => d.CreatedByUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.ToTable("Drivers");
+        }
+    }
+}
