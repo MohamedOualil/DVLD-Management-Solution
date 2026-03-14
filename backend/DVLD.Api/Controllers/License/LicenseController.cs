c510fa17be07475c94578945711fa1141f4f17fb
Mohamed Oualil <emohamedoualil@gmail.com>
2026-03-14 20:09:10 +0000

    feat(licenses): add GetLicense GET endpoint

--- backend/DVLD.Api/Controllers/License/LicenseController.cs
+++ backend/DVLD.Api/Controllers/License/LicenseController.cs
@@ -1,6 +1,7 @@
 ﻿using DVLD.Api.Common;
 using DVLD.Api.Controllers.LocalDrivingLicenseApplications;
 using DVLD.Application.Abstractions;
+using DVLD.Application.Licenses.GetLicense;
 using DVLD.Application.Licenses.GetLocalDrivingLicenseHistory;
 using DVLD.Application.LocalLicenseApplications.GetAllLocalApplications;
 using DVLD.Domain.Common;
@@ -55,6 +56,26 @@ namespace DVLD.Api.Controllers.License
 
             if (result.Value.Items.Count == 0)
                 return NotFound();
+
+            return Ok(result.Value);
+        }
+
+        [HttpGet("{LicenseId}", Name = "GetLicense")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<GetLicenseResponse>> GetLicense(
+          int LicenseId
+          , CancellationToken cancellationToken)
+        {
+            GetLicenseQuery query = new GetLicenseQuery(LicenseId);
+
+            Result<GetLicenseResponse> result = await _sender.Send(
+                query,
+                cancellationToken);
+
+            if (result.IsFailure)
+                return HandleFailure(result);
 
             return Ok(result.Value);
         }
