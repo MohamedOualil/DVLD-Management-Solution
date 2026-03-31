24cd28e1ae99f5b441a487f70c147d04ff7aa912
Mohamed Oualil <emohamedoualil@gmail.com>
2026-03-31 21:50:27 +0100

    feat(auth): implement IJwtProvider for JWT generation in infrastructure

---
+++ backend/DVLD.Infrastructure/Authentication/JwtProvider.cs
@@ -0,0 +1,44 @@
+﻿using DVLD.Application.Abstractions.Authentication;
+using DVLD.Domain.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD.Infrastructure.Authentication
+{
+    internal sealed class JwtProvider : IJwtProvider
+    {
+        public string Generate(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+
+                new Claim(ClaimTypes.Email, user.UserName),
+
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("THIS_IS_A_VERY_SECRET_KEY_123456"));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+
+            var token = new JwtSecurityToken(
+                issuer: "DVLD-Api",
+                audience: "DVLD-Clients",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
