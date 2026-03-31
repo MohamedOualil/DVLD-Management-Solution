cfdbc54df8026b93b16e0db2efd6a983b157bc1b
Mohamed Oualil <emohamedoualil@gmail.com>
2026-03-31 21:41:18 +0100

    feat(auth): add IJwtProvider abstraction for token generation

---
+++ backend/DVLD.Application/Abstractions/Authentication/IJwtProvider.cs
@@ -0,0 +1,14 @@
+﻿using DVLD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD.Application.Abstractions.Authentication
+{
+    public interface IJwtProvider
+    {
+        string Generate(User user);
+    }
+}
