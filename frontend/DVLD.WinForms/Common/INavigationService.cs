e0fbc3604c0be6a798951a53a41f4aebe317378b
Mohamed Oualil <emohamedoualil@gmail.com>
2026-04-09 14:05:17 +0100

    Add NavigationService to manage and route forms

---
+++ frontend/DVLD.WinForms/Common/INavigationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD.WinForms.Common
+{
+    public interface INavigationService
+    {
+        void ShowForm<TForm>() where TForm : Form;
+        void ShowDialog<TForm>() where TForm : Form;
+        void SetMainForm(Form mainForm);
+        void ShowChildForm<TForm>() where TForm : Form;
+    }
+}
---
+++ frontend/DVLD.WinForms/Common/NavigationService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD.WinForms.Common
+{
+    public class NavigationService : INavigationService
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private Form _form;
+
+        public NavigationService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+            
+        }
+        public void SetMainForm(Form mainForm)
+        {
+            _form = mainForm;
+        }
+
+        public void ShowChildForm<TForm>() where TForm : Form
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowDialog<TForm>() where TForm : Form
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowForm<TForm>() where TForm : Form
+        {
+            var form = _serviceProvider.GetRequiredService<TForm>();
+            form.ShowDialog();
+        }
+    }
+}
